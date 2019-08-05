1dce6ae8e2a9b3cc7cc3827ee8ca17ea77596a65
gonggongjohn <gonggongjohn@163.com>
2019-08-05 22:50:20 +0800

    Add Two Barrel Vacuum Pump Basis

--- src/main/java/com/gonggongjohn/eok/handlers/BlockHandler.java
+++ src/main/java/com/gonggongjohn/eok/handlers/BlockHandler.java
@@ -1,6 +1,7 @@
 package com.gonggongjohn.eok.handlers;
 
 import com.gonggongjohn.eok.blocks.BlockStoneTable;
+import com.gonggongjohn.eok.blocks.BlockTwoBarrelVacuumPump;
 import net.minecraft.block.Block;
 
 import java.util.ArrayList;
@@ -10,4 +11,5 @@ public class BlockHandler {
     public static final List<Block> blocks = new ArrayList<Block>();
 
     public static final Block blockStone = new BlockStoneTable();
+    public static final Block blockTwoBarrelVacuumPump = new BlockTwoBarrelVacuumPump();
 }
