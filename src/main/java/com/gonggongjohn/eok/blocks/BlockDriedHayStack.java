6ce588a8a849f48dfa3aa38f5990ea6cd89143d8
MolyOre <a64977955@163.com>
2020-01-19 12:03:30 +0800

    完成了部分放置草垛时的判定能否干燥的代码（暂时无法判断诸如玻璃等透明方块挡住的情况，现在视为无法干燥） 使用servertick事件的代码未完成（到底该怎么用啊orz）

--- src/main/java/com/gonggongjohn/eok/blocks/BlockDriedHayStack.java
+++ src/main/java/com/gonggongjohn/eok/blocks/BlockDriedHayStack.java
@@ -1,5 +1,14 @@
 package com.gonggongjohn.eok.blocks;
 
-public class BlockDriedHayStack {
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockDriedHayStack extends Block {
     public final String name= "driedHayStack";
+    public BlockDriedHayStack()
+    {
+        super(Material.GRASS);
+        this.setRegistryName(name);
+        this.setHardness(1.0F);
+    }
 }
