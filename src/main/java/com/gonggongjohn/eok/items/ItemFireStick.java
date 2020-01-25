78cf10b51597f703316a77c163eb4eb9533f1e01
NathanGX <2848391476@qq.com>
2020-01-26 06:27:07 +0800

    起火棍更新，修复了一堆bug，可以点燃指定方块了，有简单的粒子效果。玩得开心

--- src/main/java/com/gonggongjohn/eok/items/ItemFireStick.java
+++ src/main/java/com/gonggongjohn/eok/items/ItemFireStick.java
@@ -1,36 +1,30 @@
 package com.gonggongjohn.eok.items;
 
 import java.util.ArrayList;
-import javax.annotation.Nullable;
+//import java.util.Random;
 import com.gonggongjohn.eok.EOK;
 import com.gonggongjohn.eok.handlers.ItemHandler;
 import com.gonggongjohn.eok.utils.IHasModel;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-/*
- * 左手干草，右手起火器，点火
- */
-//当前，左右手持起火器（fire stick），右键点火，点燃前方方块
+//当前，左右手持起火棍（fire stick），长按右键点火
 
 public class ItemFireStick extends Item implements IHasModel {
 	
 	private final String name = "fire_stick";
 	private int cpt = 0;
 	private boolean fire = false;
+	private int useTime = 0;
+	//private Random rand;
 	
 	ArrayList<Long> timeList = new ArrayList<>();
 
@@ -42,30 +36,6 @@ public class ItemFireStick extends Item implements IHasModel {
 		this.setRegistryName(name);
 		this.setCreativeTab(EOK.tabEOK);
 		
-        this.addPropertyOverride(new ResourceLocation("pull"), new IItemPropertyGetter()
-        {
-            @SideOnly(Side.CLIENT)
-            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
-            {
-                if (entityIn == null)
-                {
-                    return 0.0F;
-                }
-                else
-                {
-                    return !(entityIn.getActiveItemStack().getItem() instanceof ItemBow) ? 0.0F : (float)(stack.getMaxItemUseDuration() - entityIn.getItemInUseCount()) / 20.0F;
-                }
-            }
-        });
-        this.addPropertyOverride(new ResourceLocation("pulling"), new IItemPropertyGetter()
-        {
-            @SideOnly(Side.CLIENT)
-            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn)
-            {
-                return entityIn != null && entityIn.isHandActive() && entityIn.getActiveItemStack() == stack ? 1.0F : 0.0F;
-            }
-        });
-		
 		ItemHandler.items.add(this);
 	}
 	
@@ -75,67 +45,100 @@ public class ItemFireStick extends Item implements IHasModel {
 		EOK.proxy.registerItemRenderer(this, 0, "inventory");
 	}
 	
-    public BlockPos getFirePos(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing) {
+    public BlockPos getFirePos(EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing) {
         
-    	pos = pos.offset(facing);
+    	pos = pos.add(0.0D, 1.0D, 0.0D);
 
         return pos;
     }
 
-	@Override									 
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+	public EnumActionResult onItemUse(EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand handIn, EnumFacing facing, float hitX, float hitY, float fitZ) {
 		
-		//Random rand = new Random(100);
+		ItemStack itemStack = playerIn.getHeldItem(handIn);
 		
-		if(cpt < 20) {
+		if(!playerIn.canPlayerEdit(pos, facing, itemStack) || fire == true) {
 			
-			timeList.add(worldIn.getWorldTime());
+			fire = false;
+
+	        return EnumActionResult.FAIL;
 		}
 		else {
 			
-			if(timeList.size() != 0) {
+			if(cpt < 20) {
 				
-				System.out.println("Total time: " + (timeList.get(timeList.size() - 1) - timeList.get(0)));
+				timeList.add(worldIn.getWorldTime());
 				
-				//cpt = 20时，连击最短用时37 
-				if((timeList.get(timeList.size() - 1) - timeList.get(0)) <= 36) {
+				if(cpt > 0) {
 					
-					fire = true;
+					//Smoke_normal
+			        worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getX() - 0.0D, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getY(), getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getZ() + 0.5D, 0.0D, 0.0D, 0.0D);
+			        worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getX() + 1.0D, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getY(), getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getZ() + 0.5D, 0.0D, 0.0D, 0.0D);
+			        worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getX() + 0.5D, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getY(), getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getZ() + 1.0D, 0.0D, 0.0D, 0.0D);
+			        worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getX() + 0.5D, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getY(), getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getZ() - 0.0D, 0.0D, 0.0D, 0.0D);
+				}
+
+				if(cpt > 10) {
+					
+					//Smoke_Large
+			        worldIn.spawnParticle(EnumParticleTypes.SMOKE_LARGE, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getX() - 0.0D, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getY(), getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getZ() + 0.5D, 0.0D, 0.0D, 0.0D);
+			        worldIn.spawnParticle(EnumParticleTypes.SMOKE_LARGE, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getX() + 1.0D, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getY(), getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getZ() + 0.5D, 0.0D, 0.0D, 0.0D);
+			        worldIn.spawnParticle(EnumParticleTypes.SMOKE_LARGE, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getX() + 0.5D, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getY(), getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getZ() + 1.0D, 0.0D, 0.0D, 0.0D);
+			        worldIn.spawnParticle(EnumParticleTypes.SMOKE_LARGE, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getX() + 0.5D, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getY(), getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getZ() - 0.0D, 0.0D, 0.0D, 0.0D);
+				}
+				
+				if(cpt > 15) {
+					
+					//Flame
+					worldIn.spawnParticle(EnumParticleTypes.FLAME, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getX() - 0.0D, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getY(), getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getZ() + 0.5D, 0.0D, 0.0D, 0.0D);
+			        worldIn.spawnParticle(EnumParticleTypes.FLAME, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getX() + 1.0D, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getY(), getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getZ() + 0.5D, 0.0D, 0.0D, 0.0D);
+			        worldIn.spawnParticle(EnumParticleTypes.FLAME, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getX() + 0.5D, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getY(), getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getZ() + 1.0D, 0.0D, 0.0D, 0.0D);
+			        worldIn.spawnParticle(EnumParticleTypes.FLAME, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getX() + 0.5D, getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getY(), getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()).getZ() - 0.0D, 0.0D, 0.0D, 0.0D);
 				}
 			}
-			
-			timeList.clear();
-			cpt = 0;
-		}
-		
-		playerIn.swingArm(EnumHand.MAIN_HAND);
-		playerIn.swingArm(EnumHand.OFF_HAND);
-	
-		if (worldIn.isRemote) {
-			
-			return super.onItemRightClick(worldIn, playerIn, handIn);
-		}
-		
-		if(playerIn.getHeldItemOffhand().getItem() == this && playerIn.getHeldItemMainhand().getItem() == this) {
-			
-			cpt++;
-
-			if(fire) {
+			else {
 				
-				//playerIn.setHeldItem(EnumHand.MAIN_HAND, ItemStack.EMPTY);
-				//playerIn.setHeldItem(EnumHand.OFF_HAND, ItemStack.EMPTY);
-				 
-				//点燃自己前方的方块
-				worldIn.setBlockState(this.getFirePos(playerIn, worldIn, playerIn.getPosition(), handIn, playerIn.getHorizontalFacing()), Blocks.FIRE.getDefaultState(), 11);
-			
-				fire = false;
-			}
-		}
-		else {
-			
-			cpt = 0;
-		}
+				if(timeList.size() != 0) {
+					
+					//System.out.println("Total time: " + (timeList.get(timeList.size() - 1) - timeList.get(0)));
+					//cpt = 20时，连击最短用时37 
+					if((timeList.get(timeList.size() - 1) - timeList.get(0)) <= 37) {
 
-		return super.onItemRightClick(worldIn, playerIn, handIn);
+						fire = true;
+					}
+				}
+				
+				timeList.clear();
+				cpt = 0;
+			}
+			
+			playerIn.swingArm(EnumHand.MAIN_HAND);
+			playerIn.swingArm(EnumHand.OFF_HAND);
+			
+			if(playerIn.getHeldItemOffhand().getItem() == this && playerIn.getHeldItemMainhand().getItem() == this) {
+				
+				cpt++;
+				
+				if(fire) {
+					
+					useTime++;
+					
+					if(useTime > 1) {
+						
+						playerIn.setHeldItem(EnumHand.MAIN_HAND, ItemStack.EMPTY);
+						playerIn.setHeldItem(EnumHand.OFF_HAND, ItemStack.EMPTY);
+						
+						useTime = 0;
+					}
+
+					worldIn.setBlockState(getFirePos(playerIn, worldIn, pos, handIn, playerIn.getHorizontalFacing()), Blocks.FIRE.getDefaultState(), 11);
+				}
+			}
+			else {
+				
+				cpt = 0;
+			}
+			
+			return EnumActionResult.SUCCESS;
+		}
 	}
 }
