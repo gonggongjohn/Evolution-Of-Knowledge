daeb44caa93542a4a32a185c952b18dd39963e4d
NathanGX <2848391476@qq.com>
2020-01-28 04:59:41 +0800

    草垛，干草垛，腐烂草垛材质更新。

--- src/main/java/com/gonggongjohn/eok/blocks/BlockDecomposedHaystack.java
+++ src/main/java/com/gonggongjohn/eok/blocks/BlockDecomposedHaystack.java
@@ -3,25 +3,33 @@ package com.gonggongjohn.eok.blocks;
 import com.gonggongjohn.eok.EOK;
 import com.gonggongjohn.eok.handlers.BlockHandler;
 import com.gonggongjohn.eok.handlers.ItemHandler;
+import com.gonggongjohn.eok.utils.IHasModel;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
-public class BlockDecomposedHaystack extends Block {
-    public final String name="decomposed_haystack";
-    public BlockDecomposedHaystack()
-    {
+public class BlockDecomposedHaystack extends Block implements IHasModel {
+	
+    public final String name = "decomposed_haystack";
+    
+    public BlockDecomposedHaystack() {
+    	
         super(Material.GRASS);
         this.setRegistryName(name);
         this.setUnlocalizedName(name);
         this.setHardness(0.3F);
         this.setCreativeTab(EOK.tabEOK);
+        this.setSoundType(SoundType.PLANT);
+        
         ItemHandler.items.add(new ItemBlock(this).setRegistryName(name));
         BlockHandler.blocks.add(this);
-        this.setSoundType(SoundType.PLANT);
     }
+    
+	@Override
+	public void registerModel() {
+		
+		EOK.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+	}
 }
--- src/main/java/com/gonggongjohn/eok/blocks/BlockDriedHaystack.java
+++ src/main/java/com/gonggongjohn/eok/blocks/BlockDriedHaystack.java
@@ -3,22 +3,33 @@ package com.gonggongjohn.eok.blocks;
 import com.gonggongjohn.eok.EOK;
 import com.gonggongjohn.eok.handlers.BlockHandler;
 import com.gonggongjohn.eok.handlers.ItemHandler;
+import com.gonggongjohn.eok.utils.IHasModel;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 
-public class BlockDriedHaystack extends Block {
-    public final String name= "dried_haystack";
-    public BlockDriedHaystack()
-    {
+public class BlockDriedHaystack extends Block implements IHasModel {
+	
+    public final String name = "dried_haystack";
+    
+    public BlockDriedHaystack() {
+    	
         super(Material.GRASS);
         this.setRegistryName(name);
         this.setHardness(0.5F);
         this.setCreativeTab(EOK.tabEOK);
         this.setUnlocalizedName(name);
         this.setSoundType(SoundType.PLANT);
+        
         BlockHandler.blocks.add(this);
         ItemHandler.items.add(new ItemBlock(this).setRegistryName(name));
     }
+    
+	@Override
+	public void registerModel() {
+		
+		EOK.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+	}
 }
