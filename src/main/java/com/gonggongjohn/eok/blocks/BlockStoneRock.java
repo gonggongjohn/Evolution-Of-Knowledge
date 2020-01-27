41e5dac22c19a435abb61545a1165c6ea4aa92c0
NathanGX <2848391476@qq.com>
2020-01-28 03:34:03 +0800

    消除了小石头紫黑方块。+ 代码强迫症改版
    碰撞箱没改，再讨论怎么弄

--- src/main/java/com/gonggongjohn/eok/blocks/BlockStoneRock.java
+++ src/main/java/com/gonggongjohn/eok/blocks/BlockStoneRock.java
@@ -3,22 +3,25 @@ package com.gonggongjohn.eok.blocks;
 import com.gonggongjohn.eok.EOK;
 import com.gonggongjohn.eok.handlers.BlockHandler;
 import com.gonggongjohn.eok.handlers.ItemHandler;
+import com.gonggongjohn.eok.utils.IHasModel;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-public class BlockStoneRock extends Block {
-    public final String name="block_stone_rock";
-    public final static AxisAlignedBB Block_Stone_Rock_AABB=new AxisAlignedBB(0.375D,0.0D,0.375D,0.625D,0.1875D,0.625D);
+public class BlockStoneRock extends Block implements IHasModel {
+	
+    public final String name = "block_stone_rock";
+    public final static AxisAlignedBB Block_Stone_Rock_AABB = new AxisAlignedBB(0.375D, 0.0D, 0.375D, 0.625D, 0.1875D, 0.625D);
 
-    public BlockStoneRock()
-    {
+    public BlockStoneRock() {
+    	
         super(Material.ROCK);
         this.setUnlocalizedName(name);
         this.setRegistryName(name);
@@ -30,30 +33,43 @@ public class BlockStoneRock extends Block {
 
     @Override
     public boolean isOpaqueCube(IBlockState state) {
+    	
         return false;
     }
 
     @Override
     public boolean isFullCube(IBlockState state) {
+    	
         return false;
     }
 
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+    	
         return Block_Stone_Rock_AABB;
     }
 
     @Override
-    public boolean canEntitySpawn(IBlockState state, Entity entityIn)
-    {
+    public boolean canEntitySpawn(IBlockState state, Entity entityIn) {
+    	
         return false;
     }
 
     @Override
-    public boolean isPassable(IBlockAccess worldIn, BlockPos pos){return true;}
+    public boolean isPassable(IBlockAccess worldIn, BlockPos pos){
+    	
+    	return true;
+    }
 
     @Override
     public void onBlockDestroyedByPlayer(World worldIn, BlockPos pos, IBlockState state) {
+    	
         worldIn.destroyBlock(pos,true);
     }
+
+	@Override
+	public void registerModel() {
+		
+		EOK.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+	}
 }
