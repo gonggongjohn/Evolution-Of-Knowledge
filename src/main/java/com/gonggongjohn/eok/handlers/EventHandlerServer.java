950722ac4fc9f1b374654e92a0bdd013d247d6a9
Os-Ir <AtomAl@163.com>
2020-02-21 17:37:24 +0800

    完成石器加工机制

--- src/main/java/com/gonggongjohn/eok/handlers/EventHandlerServer.java
+++ src/main/java/com/gonggongjohn/eok/handlers/EventHandlerServer.java
@@ -1,5 +1,7 @@
 package com.gonggongjohn.eok.handlers;
 
+import java.util.List;
+
 import com.gonggongjohn.eok.EOK;
 import com.gonggongjohn.eok.capabilities.CapabilityPlayerState;
 import com.gonggongjohn.eok.capabilities.CapabilityResearchData;
@@ -8,32 +10,69 @@ import com.gonggongjohn.eok.capabilities.IResearchData;
 import com.gonggongjohn.eok.network.PacketPlayerState;
 import com.gonggongjohn.eok.network.PacketResearchData;
 
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.Capability.IStorage;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.Clone;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-import java.util.List;
-
 @Mod.EventBusSubscriber(modid = EOK.MODID)
 public class EventHandlerServer {
+	@SubscribeEvent
+	public static void onStoneWork(RightClickBlock e) {
+		if (e.getWorld().isRemote) {
+			return;
+		}
+		if (e.getFace() != EnumFacing.UP) {
+			return;
+		}
+		BlockPos pos = e.getPos();
+		World world = e.getWorld();
+		IBlockState state = world.getBlockState(pos);
+		if (!(state.isFullBlock() && state.getMaterial() == Material.ROCK)) {
+			return;
+		}
+		ItemStack stack = e.getItemStack();
+		if (stack.getItem() != Items.FLINT) {
+			return;
+		}
+		EntityPlayer player = e.getEntityPlayer();
+		Vec3d vec = e.getHitVec();
+		vec = vec.subtract(pos.getX(), pos.getY(), pos.getZ());
+		stack.shrink(1);
+		ItemStack result;
+		if (vec.x >= 0.25 && vec.x <= 0.75 && vec.z >= 0.25 && vec.z <= 0.75) {
+			result = MetaItemsHandler.GRINDED_FLINT.getStackForm();
+		} else {
+			result = MetaItemsHandler.CHIPPED_FLINT.getStackForm();
+		}
+		player.inventory.addItemStackToInventory(result);
+		if (!result.isEmpty()) {
+			InventoryHelper.spawnItemStack(e.getWorld(), pos.getX(), pos.getY(), pos.getZ(), result);
+		}
+	}
+
 	@SubscribeEvent
 	public static void onAttachCapabilitiesEntity(AttachCapabilitiesEvent<Entity> e) {
 		if (e.getObject() instanceof EntityPlayer) {
@@ -71,12 +110,13 @@ public class EventHandlerServer {
 				IResearchData data = player.getCapability(CapabilityHandler.capResearchData, null);
 				IStorage storage = CapabilityHandler.capResearchData.getStorage();
 				List<Integer> finList = data.getFinishedResearch();
-				if(finList.isEmpty()) {
+				if (finList.isEmpty()) {
 					finList.add(1);
 					data.setFinishedResearch(finList);
 				}
 				NBTTagCompound nbt = (NBTTagCompound) storage.writeNBT(CapabilityHandler.capResearchData, data, null);
-				storage.readNBT(CapabilityHandler.capResearchData, player.getCapability(CapabilityHandler.capResearchData, null), null, nbt);
+				storage.readNBT(CapabilityHandler.capResearchData,
+						player.getCapability(CapabilityHandler.capResearchData, null), null, nbt);
 				PacketResearchData mesage = new PacketResearchData(nbt);
 				EOK.getNetwork().sendTo(mesage, player);
 			}
