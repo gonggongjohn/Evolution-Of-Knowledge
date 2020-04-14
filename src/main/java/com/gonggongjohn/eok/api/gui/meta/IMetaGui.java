b8d7e560ca59133e4392c45744a5fd9ab95a8cf6
zi-jing <3056894830@qq.com>
2020-04-14 19:36:35 +0800

    给MetaGuiScreen添加API注释

--- src/main/java/com/gonggongjohn/eok/api/gui/meta/IMetaGui.java
+++ src/main/java/com/gonggongjohn/eok/api/gui/meta/IMetaGui.java
@@ -1,11 +1,41 @@
 package com.gonggongjohn.eok.api.gui.meta;
 
+import net.minecraft.client.Minecraft;
+
 public interface IMetaGui {
 	
 	/**
-	 * 获取这个Gui的所有控件及其对应的ID。<br>
-	 * Get all the controls of this Gui and their corresponding IDs.
+	 * 获取这个Gui的所有控件及其对应的ID。<br><br>
+	 * Gets all the controls of this Gui and their corresponding IDs.
 	 */
 	public ControlMap getControls();
+	
+	/**
+	 * 将一个控件添加至Gui中。<br><br>
+	 * Adds a control to this Gui.
+	 */
+	public void addControl(GuiControl control);
+	
+	/**
+	 * 将一个控件从Gui中移除。<br><br>
+	 * Removes a control from this Gui.
+	 */
+	public void removeControl(GuiControl control);
+	
+	/**
+	 * 获取游戏窗口的宽。<br><br>
+	 * Get the width of the game screen。
+	 */
+	default public int getScreenWidth() {
+		return Minecraft.getMinecraft().displayWidth;
+	}
+
+	/**
+	 * 获取游戏窗口的高。<br><br>
+	 * Get the height of the game screen
+	 */
+	default public int getScreenHeight() {
+		return Minecraft.getMinecraft().displayHeight;
+	}
 	
 }
--- src/main/java/com/gonggongjohn/eok/api/gui/meta/MetaGuiScreen.java
+++ src/main/java/com/gonggongjohn/eok/api/gui/meta/MetaGuiScreen.java
@@ -17,33 +17,40 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.ResourceLocation;
 
 /**
+ * GuiScreen基类<br><br>
  * GuiScreen Base Class
  * 
  * @see GuiScreenTest
  */
-public abstract class MetaGuiScreen  extends GuiScreen implements IMetaGui {
+public abstract class MetaGuiScreen extends GuiScreen implements IMetaGui {
 	/**
+	 * 这个Map用来存储所有的控件，键为控件ID，值为控件的实例。<br><br>
 	 * This map is used to store all Gui controls. The key is the control ID and the
 	 * value is the control object.
 	 */
 	private ControlMap controls = new ControlMap(this);
 	private ResourceLocation TEXTURE = new ResourceLocation("");
 	/**
+	 * 这个实例用来创建控件。<br>
 	 * This object is used to create Controls.<br><br>
-	 * Example: <br><br><code>builder.new GuiButton(...);</code>
-	 * 
-	 * @see GuiScreenTest
+	 * 例子/Example: <br><code>builder.createButton(...);</code><br><br>
+	 * <strong>注意/Warning: <br>要使控件被添加进Gui中，必须使用<code>this.addControl(...);
+	 * </code>来添加。</strong><br>
+	 * <br>If you want to add a control to this Gui, you must use 
+	 * <code>this.addControl(...);</code>.</strong>
 	 */
 	public GuiControl.ControlBuilder builder = new GuiControl.ControlBuilder(this);
 	private int nextId = 0;
 	/**
+	 * 这里的窗口宽度不是游戏窗口宽度，是Gui的宽度。<br><br>
 	 * The window width here is not the width of the game window, but the width of
-	 * the Gui interface
+	 * the Gui interface.
 	 */
 	private int windowWidth;
 	/**
+	 * 这里的窗口高度不是游戏窗口高度，是Gui的高度。<br><br>
 	 * The window height here is not the height of the game window, but the height
-	 * of the Gui interface
+	 * of the Gui interface.
 	 */
 	private int windowHeight;
 	private int texWidth;
@@ -51,18 +58,23 @@ public abstract class MetaGuiScreen  extends GuiScreen implements IMetaGui {
 	private int offsetX;
 	private int offsetY;
 	/**
+	 * 它用来决定在Gui被打开是是否强制调小游戏界面缩放设置。<br><br>
 	 * It is used to decide whether or not to force the interface zoom factor to be
-	 * changed when the Gui is opened
+	 * turned down when the Gui is opened.
 	 */
 	private boolean forceChangeGuiScale;
 	/**
+	 * 它用来决定Gui是否有自定义背景，如果它为true, <code>drawDefaultBackground()</code>
+	 * 方法将不会被自动执行。<br><br>
 	 * It is used to determine whether the Gui has a custom background If it is
-	 * true, the drawDefaultBackground() method will not be executed
+	 * true, the <code>drawDefaultBackground()</code> method will not be executed 
+	 * automaticly.
 	 */
 	private boolean hasCustomBackground = false;
 	/**
+	 * 存储玩家打开Gui前的界面缩放设置，以便于在玩家关闭Gui时恢复到原来的设置。<br><br>
 	 * Store the interface zoom settings before the player opens the Gui, so as to
-	 * restore the original interface size when the player closes the Gui
+	 * restore the original interface size when the player closes the Gui.
 	 */
 	private int lastGuiScale;
 
@@ -158,11 +170,19 @@ public abstract class MetaGuiScreen  extends GuiScreen implements IMetaGui {
 		return this.controls;
 	}
 
-	private static void throwArgumentException(String s) {
-		EOK.getLogger().error("An error occurred when rendering Gui, " + s + ".");
-		throw new IllegalArgumentException(s);
+	/**
+	 * 抛出IllegalArgumentException并输出报错信息。<br><br>
+	 * Throws an IllegalArgumentException and prints this error message. 
+	 */
+	private static void throwArgumentException(String message) {
+		EOK.getLogger().error("An error occurred when rendering Gui, " + message + ".");
+		throw new IllegalArgumentException(message);
 	}
 	
+	/**
+	 * 获取这个Gui的贴图位置。<br><br>
+	 * Gets the texture location of this Gui.
+	 */
 	public ResourceLocation getTexture() {
 		return TEXTURE;
 	}
@@ -175,14 +195,27 @@ public abstract class MetaGuiScreen  extends GuiScreen implements IMetaGui {
 		return this.texHeight;
 	}
 
+	/**
+	 * 获取界面的实际宽度。<br><br>
+	 * Gets the real width of this Gui.
+	 */
 	public int getWindowWidth() {
 		return windowWidth;
 	}
 
+	/**
+	 * 获取界面的实际高度。<br><br>
+	 * Gets the real height of this Gui.
+	 */
 	public int getWindowHeight() {
 		return windowHeight;
 	}
 
+	/**
+	 * 设置界面的实际大小，<strong>不建议在构造函数/initGui()之外使用。</strong><br><br>
+	 * Sets the real width of this Gui, <strong>it's not recommended to use this 
+	 * outside of the constructor/initGui().</strong>
+	 */
 	public void setWindowSize(int width, int height) {
 		if (width <= 0 || height <= 0) {
 			throwArgumentException("width and height must be positive");
@@ -192,19 +225,26 @@ public abstract class MetaGuiScreen  extends GuiScreen implements IMetaGui {
 	}
 
 	/**
-	 * Get the abscissa of the top left corner of Gui
+	 * 获取Gui左上角的横坐标。<br><br>
+	 * Gets the abscissa of the top left corner of Gui.
 	 */
 	public int getOffsetX() {
 		return offsetX;
 	}
 
 	/**
-	 * Get the vertical coordinate of the top left corner of Gui
+	 * 获取Gui左上角的纵坐标。<br><br>
+	 * Gets the vertical coordinate of the top left corner of Gui.
 	 */
 	public int getOffsetY() {
 		return offsetY;
 	}
 
+	/**
+	 * 指定贴图大小，<strong>不建议在构造函数/initGui()之外使用。</strong><br><br>
+	 * Appoints the texture size, <strong>it's not recommended to use this 
+	 * outside of the constructor/initGui().</strong>
+	 */
 	public void setTextureSize(int width, int height) {
 		if (width <= 0 || height <= 0) {
 			throwArgumentException("texture width and texture height must be positive");
@@ -213,21 +253,29 @@ public abstract class MetaGuiScreen  extends GuiScreen implements IMetaGui {
 		this.texHeight = height;
 	}
 
+	/**
+	 * 设置这个Gui的贴图位置。<br><br>
+	 * Sets the texture location of this Gui.
+	 */
 	public void setTexture(ResourceLocation texture) {
 		if (texture == null)
 			throwArgumentException("texture can't be null");
 		TEXTURE = texture;
 	}
 
+	/**
+	 * 获取这个Gui的标题。<br><br>
+	 * Gets the title of this Gui.
+	 */
 	public String getTitle() {
 		return title;
 	}
 
 	/**
+	 * 设置显示在界面上边中间的标题，<strong>不支持多行</strong>。<br><br>
 	 * Set the title of the interface, which is displayed in the middle of the upper
-	 * part of the interface by default
+	 * part of the interface by default, <strong>doesn't support multi line yet</strong>.
 	 * 
-	 * @param title Title, doesn't support multi line yet
 	 */
 	public void setTitle(String title) {
 		if (title == null)
@@ -236,6 +284,7 @@ public abstract class MetaGuiScreen  extends GuiScreen implements IMetaGui {
 	}
 
 	/**
+	 * 获取Gui在被打开时是否会强制修改MC界面缩放设置。<br><br>
 	 * Gets whether the Gui will force the interface scaling settings to be modified
 	 * when it is opened
 	 */
@@ -243,23 +292,12 @@ public abstract class MetaGuiScreen  extends GuiScreen implements IMetaGui {
 		return forceChangeGuiScale;
 	}
 
-	/*
-	 * Get the width of the game screen
-	 */
-	public int getScreenWidth() {
-		return width;
-	}
-
 	/**
-	 * Get the height of the game screen
-	 */
-	public int getScreenHeight() {
-		return height;
-	}
-
-	/**
-	 * Set whether this Gui has a custom background If it is true, the
-	 * drawDefaultBackground() method will not be executed
+	 * 指定Gui是否有自定义背景，如果它为true, <code>drawDefaultBackground()</code>
+	 * 方法将不会被自动执行。<br><br>
+	 * Appoints whether the Gui has a custom background If it is
+	 * true, the <code>drawDefaultBackground()</code> method will not be executed 
+	 * automaticly.
 	 */
 	public void setHasCustomBackground(boolean b) {
 		this.hasCustomBackground = b;
@@ -269,10 +307,12 @@ public abstract class MetaGuiScreen  extends GuiScreen implements IMetaGui {
 		return this.buttonList;
 	}
 	
+	@Override
 	public void addControl(GuiControl control) {
 		this.controls.put(nextId++, control);
 	}
 	
+	@Override
 	public void removeControl(GuiControl control) {
 		this.controls.remove(control.getId());
 	}
