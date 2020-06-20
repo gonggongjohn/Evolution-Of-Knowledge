d81143d3f3496007d9382759fd7ee0cd4f08f51d
Mo-Ink <2350745751@qq.com>
2020-06-20 13:20:58 +0800

    添加红石盔甲

---
+++ src/main/java/com/gonggongjohn/eok/items/ItemRedstoneBoots.java
@@ -0,0 +1,24 @@
+package com.gonggongjohn.eok.items;
+
+import com.gonggongjohn.eok.EOK;
+import com.gonggongjohn.eok.handlers.ItemHandler;
+import com.gonggongjohn.eok.utils.IHasModel;
+import net.minecraft.inventory.EntityEquipmentSlot;
+
+public class ItemRedstoneBoots extends ItemRedstoneArmor implements IHasModel {
+    private final String name = "redstone_boots";
+
+    public ItemRedstoneBoots() {
+        super(EntityEquipmentSlot.FEET);
+        this.setUnlocalizedName("eok." + name);
+        this.setRegistryName(name);
+        this.setCreativeTab(EOK.tabEOK);
+        this.setMaxStackSize(1);
+        ItemHandler.ITEM_REGISTRY.add(this);
+    }
+
+    @Override
+    public void registerModel() {
+        EOK.proxy.registerItemRenderer(this, 0, "inventory");
+    }
+}
