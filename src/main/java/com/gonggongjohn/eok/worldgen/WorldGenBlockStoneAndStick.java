3ab60af30946b9cb27f21ac37160317b0ffb419f
Mo-Ink <2350745751@qq.com>
2020-07-05 18:11:06 +0800

    分离WorldGenRedstoneAppleTree

--- src/main/java/com/gonggongjohn/eok/worldgen/WorldGenBlockStoneAndStick.java
+++ src/main/java/com/gonggongjohn/eok/worldgen/WorldGenBlockStoneAndStick.java
@@ -2,10 +2,6 @@ package com.gonggongjohn.eok.worldgen;
 
 import com.gonggongjohn.eok.handlers.BlockHandler;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockOldLeaf;
-import net.minecraft.block.BlockOldLog;
-import net.minecraft.block.BlockPlanks;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -16,89 +12,30 @@ import net.minecraftforge.fml.common.IWorldGenerator;
 import java.util.Random;
 
 public class WorldGenBlockStoneAndStick implements IWorldGenerator {
-	
-	private static final IBlockState LOG = Blocks.LOG.getDefaultState().withProperty(BlockOldLog.VARIANT, BlockPlanks.EnumType.OAK);
-    private static final IBlockState LEAF = Blocks.LEAVES.getDefaultState().withProperty(BlockOldLeaf.VARIANT, BlockPlanks.EnumType.OAK).withProperty(BlockOldLeaf.CHECK_DECAY, Boolean.FALSE);
-	
-	@Override
-	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
-			IChunkProvider chunkProvider) {
-		for (int i = 0; i < 3; i++) {
-			BlockPos pos = new BlockPos(chunkX * 16 + 8 + random.nextInt(15), 0, chunkZ * 16 + 8 + random.nextInt(15));
-			pos = world.getTopSolidOrLiquidBlock(pos).down();
-			if (this.canSustainSt(world, pos)) {
-				world.setBlockState(pos.up(), BlockHandler.blockStoneRock.getDefaultState());
-			}
-		}
-		for (int i = 0; i < 2; i++) {
-			BlockPos pos = new BlockPos(chunkX * 16 + 8 + random.nextInt(15), 0, chunkZ * 16 + 8 + random.nextInt(15));
-			pos = world.getTopSolidOrLiquidBlock(pos).down();
-			if (this.canSustainSt(world, pos)) {
-				world.setBlockState(pos.up(), BlockHandler.blockStick.getDefaultState());
-			}
-		}
-		
-		//generate apple trees
-		for (int i = 0; i < 3; i++) {
-			
-			BlockPos pos = new BlockPos(chunkX * 16 + 8 + random.nextInt(15), 0, chunkZ * 16 + 8 + random.nextInt(15));
-			pos = world.getTopSolidOrLiquidBlock(pos).down();
-				
-			if(this.canSustainSt(world, pos)) {
-				
-				for(int i2 = pos.getY() - 3 + i; i2 <= pos.getY() + i; ++i2) {
-					
-                    int k2 = i2 - (pos.getY() + i);
-                    int l2 = 1 - k2 / 2;
 
-                    for (int i3 = pos.getX() - l2; i3 <= pos.getX() + l2; ++i3)
-                    {
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
+                         IChunkProvider chunkProvider) {
+        for (int i = 0; i < 3; i++) {
+            BlockPos pos = new BlockPos(chunkX * 16 + 8 + random.nextInt(15), 0, chunkZ * 16 + 8 + random.nextInt(15));
+            pos = world.getTopSolidOrLiquidBlock(pos).down();
+            if (this.canSustainSt(world, pos)) {
+                world.setBlockState(pos.up(), BlockHandler.blockStoneRock.getDefaultState());
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            BlockPos pos = new BlockPos(chunkX * 16 + 8 + random.nextInt(15), 0, chunkZ * 16 + 8 + random.nextInt(15));
+            pos = world.getTopSolidOrLiquidBlock(pos).down();
+            if (this.canSustainSt(world, pos)) {
+                world.setBlockState(pos.up(), BlockHandler.blockStick.getDefaultState());
+            }
+        }
+    }
 
-                        for (int k1 = pos.getZ() - l2; k1 <= pos.getZ() + l2; ++k1)
-                        {     
-                            
-                            if(i2 + 5 > pos.getY() + 3 && i2 + 5 < pos.getY() + 5) {
-                            	
-                                BlockPos fruitPos = new BlockPos(i3, i2 + 5, k1);
-                                
-                                if(random.nextInt(5) == 1) {
-                                	
-                                    world.setBlockState(fruitPos, BlockHandler.blockApple.getDefaultState());
-                                }
-                            }
-                        }
-                    }
-                }
-				
-				for (int i2 = pos.getY() - 3 + i; i2 <= pos.getY() + i; ++i2)
-                {
-                    int k2 = i2 - (pos.getY() + i);
-                    int l2 = 1 - k2 / 2;
-
-                    for (int i3 = pos.getX() - l2; i3 <= pos.getX() + l2; ++i3)
-                    {
-
-                        for (int k1 = pos.getZ() - l2; k1 <= pos.getZ() + l2; ++k1)
-                        {                         
-                            BlockPos blockpos = new BlockPos(i3, i2 + 6, k1);
-                            
-                            world.setBlockState(blockpos, LEAF);
-                        }
-                    }
-                }
-				
-				for(int j = 0; j < 6; j++) {
-					
-					world.setBlockState(pos.up(j), LOG);
-				}
-			}
-		}
-	}
-
-	private boolean canSustainSt(World world, BlockPos pos) {
-		Block block = world.getBlockState(pos).getBlock();
-		boolean flag1 = world.isAirBlock(pos.up()) || world.getBlockState(pos.up()).getBlock() == Blocks.SNOW_LAYER;
-		boolean flag2 = block == Blocks.GRASS || block == Blocks.STONE || block == Blocks.SAND;
-		return flag1 & flag2;
-	}
+    private boolean canSustainSt(World world, BlockPos pos) {
+        Block block = world.getBlockState(pos).getBlock();
+        boolean flag1 = world.isAirBlock(pos.up()) || world.getBlockState(pos.up()).getBlock() == Blocks.SNOW_LAYER;
+        boolean flag2 = block == Blocks.GRASS || block == Blocks.STONE || block == Blocks.SAND;
+        return flag1 & flag2;
+    }
 }
