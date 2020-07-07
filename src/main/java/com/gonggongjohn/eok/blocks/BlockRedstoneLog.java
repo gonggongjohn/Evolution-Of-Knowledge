fe3ab216fa77e9ee00b05fbe95b6bb1c24b918a8
Mo-Ink <2350745751@qq.com>
2020-07-07 16:56:00 +0800

    RedstoneAppleTree完成35%

--- src/main/java/com/gonggongjohn/eok/blocks/BlockRedstoneLog.java
+++ src/main/java/com/gonggongjohn/eok/blocks/BlockRedstoneLog.java
@@ -4,13 +4,16 @@ import com.gonggongjohn.eok.EOK;
 import com.gonggongjohn.eok.api.render.ICustomModel;
 import com.gonggongjohn.eok.handlers.BlockHandler;
 import com.gonggongjohn.eok.handlers.ItemHandler;
+import com.gonggongjohn.eok.utils.IHasModel;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockLog;
 import net.minecraft.block.BlockOldLog;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
@@ -21,22 +24,23 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 
-public class BlockRedstoneLog extends Block {
+public class BlockRedstoneLog extends Block implements IHasModel {
 
     public final String name = "redstone_log";
 
     public BlockRedstoneLog() {
-
         super(Material.WOOD);
-        this.setUnlocalizedName("eok." + name);
         this.setRegistryName(name);
+        this.setUnlocalizedName("eok." + name);
         this.setHardness(2.0F);
         this.setSoundType(SoundType.WOOD);
         this.setCreativeTab(EOK.tabEOK);
-        BlockHandler.registerBlock(this, new ItemBlock(this).setRegistryName(name));
+        BlockHandler.BLOCK_REGISTRY.add(this);
+        ItemHandler.ITEM_REGISTRY.add(new ItemBlock(this).setRegistryName(name));
     }
 
-    @Override public boolean canSustainLeaves(IBlockState state, net.minecraft.world.IBlockAccess world, BlockPos pos){ return true; }
-    @Override public boolean isWood(net.minecraft.world.IBlockAccess world, BlockPos pos){ return true; }
-
+    @Override
+    public void registerModel() {
+        EOK.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+    }
 }
