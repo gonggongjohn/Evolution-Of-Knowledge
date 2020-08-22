4ee5f6dfcf652cb25077a0b9bdd8d3396b3cdd42
Mo_Ink <64246616+Mo-Ink@users.noreply.github.com>
2020-08-22 17:56:43 +0800

    Add Redstone Tools

---
+++ src/main/java/com/gonggongjohn/eok/tier/RedstoneTier.java
@@ -0,0 +1,37 @@
+package com.gonggongjohn.eok.tier;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+
+public class RedstoneTier implements IItemTier {
+    @Override
+    public int getMaxUses() {
+        return 222;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return 13.5F;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 2.2F;
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return 3;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 12;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return Ingredient.fromItems(Items.REDSTONE);
+    }
+}
