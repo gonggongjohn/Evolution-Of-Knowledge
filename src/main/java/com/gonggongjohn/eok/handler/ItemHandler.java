5f316b59c026defde1b4786c8d2f8da20cf6c241
Mo_Ink <64246616+Mo-Ink@users.noreply.github.com>
2020-08-22 17:56:47 +0800

    Add Redstone Tools

--- src/main/java/com/gonggongjohn/eok/handler/ItemHandler.java
+++ src/main/java/com/gonggongjohn/eok/handler/ItemHandler.java
@@ -1,8 +1,7 @@
 package com.gonggongjohn.eok.handler;
 
 import com.gonggongjohn.eok.EOK;
-import com.gonggongjohn.eok.item.EOKManualItem;
-import com.gonggongjohn.eok.item.EOKSymbolItem;
+import com.gonggongjohn.eok.item.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.Rarity;
 
@@ -15,6 +14,11 @@ public class ItemHandler {
     public static final Item itemEOKSymbol = register("eok_symbol", new EOKSymbolItem());
     public static final Item itemEOKManual = register("eok_manual", new EOKManualItem());
     public static final Item itemRedstoneApple = register("redstone_apple", new Item((new Item.Properties()).group(EOK.EOK_ITEMGROUP).rarity(Rarity.RARE).food(FoodHandler.REDSTONE_APPLE)));
+    public static final Item itemRedstoneAxe = register("redstone_axe", new RedstoneAxeItem());
+    public static final Item itemRedstoneHoe = register("redstone_hoe", new RedstoneHoeItem());
+    public static final Item itemRedstoneShovel = register("redstone_shovel", new RedstoneShovelItem());
+    public static final Item itemRedstoneSword = register("redstone_sword", new RedstoneSwordItem());
+    public static final Item itemRedstonePickaxe = register("redstone_pickaxe", new RedstonePickaxeItem());
 
     private static Item register(String unlocalizedName, Item item) {
         item.setRegistryName(EOK.MODID, unlocalizedName);
