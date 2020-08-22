3d7fcc1978d892cfd32d8b1d9910ec9db2da2afd
Mo_Ink <64246616+Mo-Ink@users.noreply.github.com>
2020-08-22 16:44:30 +0800

    Add Redstone Apple

---
+++ src/main/java/com/gonggongjohn/eok/handler/FoodHandler.java
@@ -0,0 +1,16 @@
+package com.gonggongjohn.eok.handler;
+
+import net.minecraft.item.Food;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+
+public class FoodHandler {
+    public static final Food REDSTONE_APPLE = (new Food.Builder())
+            .hunger(4)
+            .saturation(0.6F)
+            .effect(new EffectInstance(Effects.ABSORPTION, 600, 1), 1.0F)
+            .effect(new EffectInstance(Effects.SATURATION, 100, 0), 1.0F)
+            .effect(new EffectInstance(Effects.REGENERATION, 160, 0), 1.0F)
+            .setAlwaysEdible()
+            .build();
+}
--- src/main/java/com/gonggongjohn/eok/handler/ItemHandler.java
+++ src/main/java/com/gonggongjohn/eok/handler/ItemHandler.java
@@ -4,6 +4,7 @@ import com.gonggongjohn.eok.EOK;
 import com.gonggongjohn.eok.item.EOKManualItem;
 import com.gonggongjohn.eok.item.EOKSymbolItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.Rarity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,7 @@ public class ItemHandler {
 
     public static final Item itemEOKSymbol = register("eok_symbol", new EOKSymbolItem());
     public static final Item itemEOKManual = register("eok_manual", new EOKManualItem());
+    public static final Item itemRedstoneApple = register("redstone_apple", new Item((new Item.Properties()).group(EOK.EOK_ITEMGROUP).rarity(Rarity.RARE).food(FoodHandler.REDSTONE_APPLE)));
 
     private static Item register(String unlocalizedName, Item item) {
         item.setRegistryName(EOK.MODID, unlocalizedName);
